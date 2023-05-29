53e58ebf6fa49110af92e73e9073babb866315d1
Your Name <email@example.com>
2023-05-29 18:19:42 +0800

    complete the git

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageButton button_first;
+    private ImageButton button_second;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button_first = findViewById(R.id.btn1);
+        button_second = findViewById(R.id.btn2);
+        button_second.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,CompareSystem.class);
+                startActivity(intent);
+            }
+        });
+        button_first.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,SearchActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
